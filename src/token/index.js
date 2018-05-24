7da10da18c540eb5bcf59a05f07220f7d63f6eb7
Yuqing Lee <im.yuqlee@gmail.com>
2018-05-24 16:16:07 +0800

    Transfer assets from bot

---
+++ src/token/index.js
@@ -0,0 +1,46 @@
+import './index.scss';
+import $ from 'jquery';
+import validator from 'validator';
+import FormUtils from '../utils/form.js';
+
+function Token(router, api) {
+  this.router = router;
+  this.api = api;
+  this.templateIndex = require('./index.html');
+}
+
+Token.prototype = {
+  index: function (id) {
+    const self = this;
+    let objStr = window.localStorage.getItem(id);
+    let data = JSON.parse(objStr);
+    if (data == undefined) {
+      data = {};
+    }
+
+    $('body').attr('class', 'token layout');
+    $('#layout-container').html(self.templateIndex(data));
+    $('form').on('submit', function (event) {
+      event.preventDefault();
+
+      var params = new FormUtils().serialize($(this));
+      if (!validator.isUUID(params['client_id'], 4)) {
+        self.api.notify('error', 'Client Id Format Error');
+        return
+      }
+      if (!validator.isUUID(params['session_id'], 4)) {
+        self.api.notify('error', 'Session Id Format Error');
+        return
+      }
+      if (!validator.isBase64(params['pin_token'])) {
+        self.api.notify('error', 'Pin Token Format Error');
+        return
+      }
+      window.localStorage.setItem(id, JSON.stringify(params));
+      self.router.replace("/apps/"+id);
+    });
+    self.router.updatePageLinks();
+  }
+};
+
+export default Token;
