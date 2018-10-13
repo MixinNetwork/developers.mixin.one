95147345ac716d1d69e9244bd7b1ccd44e7c6404
Li Yuqing <im.yuqlee@gmail.com>
2018-10-13 18:16:19 +0800

    Fix deploy.sh and layout bug

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ cd api
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config-production.yml || exit
 cd ..
 mkdir dist/api
-mv api/_site/beta-mixin-message api/_site/alpha-mixin-network api/_site/css api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
-favicon=$(sed -ne "s/\(\<link.*head\>\)/\1/p" dist/index.html)
-sed -i '' 's|\<\/head\>|'"$favicon"'|g' ./dist/api/index.html
+mv api/_site/beta-mixin-message api/_site/alpha-mixin-network api/_site/css api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
 
 rsync -rcv dist/* one@mixin-developers:html/
