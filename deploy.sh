d0ab8179d1b60f1e76e34320b7490f4831669f9b
Neosky <her_liu@qq.com>
2019-12-11 15:50:26 +0800

    add yarn.lock

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ mkdir dist/api
 mv api/_site/beta-mixin-message api/_site/alpha-mixin-network api/_site/assets api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
 cd src/client
 ./build.sh
+cd ../..
 rsync -rcv dist/* one@mixin-developers:html/
