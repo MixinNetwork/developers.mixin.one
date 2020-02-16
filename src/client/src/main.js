09593ef284f3d0d39a2f80fb6fa0650d3f6bbfee
liuzemei <her_liu@qq.com>
2020-02-16 18:01:29 +0800

    delete test token

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -9,11 +9,12 @@ import VueTouch from 'vue-touch'
 import store from './store'
 import VueClipboard from 'vue-clipboard2'
 import i18n from './assets/i18n'
-import elementUI from 'element-ui'
+import { Message, MessageBox, Loading } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(elementUI)
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$message = Message;
 
-
+Vue.use(Loading.directive);
 Vue.use(VueClipboard)
 Vue.use(VueTouch, { name: 'v-touch' })
 VueTouch.config.swipe = {
@@ -27,7 +28,6 @@ Vue.prototype.apis = apis
 Vue.prototype._const = _const
 Vue.prototype.$bus = $bus
 
-window.localStorage.setItem('token', 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiJiOTE5ZTAwZi0zYWQ0LTQxNjktYjk3NC1lYTRkMDVlZDI1YzQiLCJleHAiOjE2MTMzNTk1OTYsImlhdCI6MTU4MTgyMzU5NiwiaXNzIjoiZmJkMjZiYzYtM2QwNC00OTY0LWE3ZmUtYTU0MDQzMmIxNmUyIn0.IP-5Yn0_uppXhR00SPfRbC5VbbW3pvclxs-IgFpuSA7PMxmzKSc1zJFFmPulZ2obneSIO3mVs98B0cg5tT6fJvV3jbzsKSpKeWvl_96HTJhBE8odpvdVhQTs8RyrWNgjrUBTSj2Zhc842qqeeGnKc23b1lPsvRfPrC8DehCvA1w')
 new Vue({
   render: h => h(App),
   router,
