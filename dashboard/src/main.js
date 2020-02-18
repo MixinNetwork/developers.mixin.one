7511bd8228653dfa9a51fc842a7676aadcbb157b
liuzemei <her_liu@qq.com>
2020-02-18 09:53:35 +0800

    Directory optimization

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -22,8 +22,6 @@ Vue.prototype.apis = apis
 Vue.prototype._const = _const
 Vue.prototype.$bus = $bus
 
-window.localStorage.setItem('token', 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiI5MzhkMGMwOC1kMTBhLTRkYTItYTlmMy03NGNmZDVhODA3YjciLCJleHAiOjE2MTM0MzgwNjEsImlhdCI6MTU4MTkwMjA2MSwiaXNzIjoiZmJkMjZiYzYtM2QwNC00OTY0LWE3ZmUtYTU0MDQzMmIxNmUyIn0.W_Xq_V3KJ0N2wHUJGq1OJ6Fj8t4phEe65zD0gpbQ6-KqPf7Ex6t7YAlvEheGQu0ieOqYExtCxqq4sC6_d69ItxfLqp2_u6hovYNwzLz1koHu5laMa0arBM4i-t05JnlRjctPq_7XkmfOs0_xf90I_Q62tbLH7rbuOGZyCxCnXWY')
-
 new Vue({
   render: h => h(App),
   router,
