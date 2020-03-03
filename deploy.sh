03460a4ba8589735f4804a603aa807ca58b5551d
Li Yuqing <im.yuqlee@gmail.com>
2020-03-03 21:16:27 +0800

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ bundle install
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config-production.yml || exit
 cd ..
 mkdir dist/api
-mv api/_site/beta-mixin-message api/_site/alpha-mixin-network api/_site/assets api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
+#mv api/_site/assets api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
+mv api/_site/* dist/api/
 
 cd dashboard
 yarn build
