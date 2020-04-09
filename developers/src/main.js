0543088d12c692d9a7e2c1ae03b2d891f670b05a
neo <neo@neodeMac-mini.local>
2020-04-09 11:54:06 +0800

    delete token

--- developers/src/main.js
+++ developers/src/main.js
@@ -13,8 +13,6 @@ import 'croppie/croppie.css'
 
 Vue.prototype.$message = Message;
 
-window.localStorage.setItem('token', 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhaWQiOiJkOWIzNGU4Mi00NjQ3LTQwZTItYWFhZC03MTdiNTdkZWViMGIiLCJleHAiOjE2MTc0NTczMzMsImlhdCI6MTU4NTkyMTMzMywiaXNzIjoiZmJkMjZiYzYtM2QwNC00OTY0LWE3ZmUtYTU0MDQzMmIxNmUyIn0.ETlvTGQReBXey4_RJAdv4N3dX_IwHGoOuLP-4sdU_WwPwIow7RMe9Sn7DydcIYYVgVNJJlNs7kG-xo0Jnw10Y4c5Fg0MDJVCjo9lahgPMOTGMv9tdHvIk_wXRT9APqvQi9Rql3nlABXtERXRo-2AZie6l8Ugcv1ynkU90YW4JmY')
-
 Vue.use(Loading.directive);
 Vue.use(VueClipboard)
 Vue.use(VueCroppie);
