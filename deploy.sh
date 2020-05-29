413c249fce73ce1ac50868246eff016954387a9c
Li Yuqing <im.yuqlee@gmail.com>
2020-05-29 19:03:22 +0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ mkdir dist
 cd api
 bundle install
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config-production.yml || exit
-md5=$(md5sum "api/_site/css/main.css" | cut -d ' ' -f 1)
-sed -i ''  "s/main.css/main.css?v=$md5/g" api/_site/index.html || exit
 cd ..
+md5=$(md5sum "./api/_site/css/main.css" | cut -d ' ' -f 1)
+sed -i ''  "s/\/css\/main.css/\/api\/css\/main.css?v=$md5/g" ./api/_site/index.html || exit
 mkdir dist/api
 #mv api/_site/assets api/_site/scripts api/_site/images api/_site/search api/_site/favicon.ico api/_site/apple-touch-icon.png api/_site/launcher.png api/_site/robots.txt api/_site/index.html dist/api/
 mv api/_site/* dist/api/
