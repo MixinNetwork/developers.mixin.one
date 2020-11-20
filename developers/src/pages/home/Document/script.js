234774f6333f1f359958e825c9b95e0480423a4c
Neo <her_liu@qq.com>
2020-11-20 19:09:43 +0800

    use sticky

--- developers/src/pages/home/Document/script.js
+++ developers/src/pages/home/Document/script.js
@@ -26,16 +26,12 @@ export default {
     let { pathMatch } = this.$route.params
     handlePathInit.call(this, pathMatch)
   },
-  beforeDestroy() {
-    window.removeEventListener('scroll', navAutoMove)
-  }
 }
 
 function handlePathInit(pathMatch) {
   this.$nextTick(() => {
     require("@/assets/js/animate-up").default()
     handleCodeHighLight()
-    window.addEventListener('scroll', navAutoMove)
     document.documentElement.scrollTop = 0
   })
   pathMatch = pathMatch ? pathMatch.substr(1) : "mainnet/overview"
@@ -49,21 +45,4 @@ function handlePathInit(pathMatch) {
   if (!isFind) return console.log('no doc')
   isFind = langCheck.some(item => getPageContent.call(this, item))
   if (!isFind) console.log('no doc2')
-}
-
-const baseHeight = 100
-
-function navAutoMove() {
-  const menuDOM = document.getElementsByClassName('one-path')[0]
-  const arcticDom = document.getElementsByClassName('container')[0]
-  const arcticHeight = arcticDom.clientHeight
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-  let menuHeight = menuDOM.clientHeight
-  if (scrollTop <= baseHeight) {
-    menuDOM.style.marginTop = '0px'
-  } else if (scrollTop > baseHeight + arcticHeight - menuHeight) {
-    menuDOM.style.marginTop = arcticHeight - menuHeight
-  } else {
-    menuDOM.style.marginTop = scrollTop - baseHeight + 'px'
-  }
 }
