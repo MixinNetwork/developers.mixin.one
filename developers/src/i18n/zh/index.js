f610a2b55d42b692bcc0c81f6e4858ae95e1e8b6
Jun Tang <tangjun@mixin.one>
2020-12-22 19:54:09 +0800

    Update mtg doc

--- developers/src/i18n/zh/index.js
+++ developers/src/i18n/zh/index.js
@@ -185,15 +185,12 @@ export default {
           { name: "价格", path: "mainnet/concepts/price", router: "mainnet/concepts/price" },
           { name: "D3M-PIN", path: "mainnet/concepts/d3m-pin", router: "mainnet/concepts/d3m-pin" },
           { name: "全节点", path: "mainnet/concepts/full-node", router: "mainnet/concepts/full-node" },
-          /*
           { name: "MTG", path: "mainnet/concepts/mtg", router: "mainnet/concepts/mtg" },
+          /*
           { name: "DAG", path: "mainnet/concepts/dag", router: "mainnet/concepts/dag" },
           { name: "TEE", path: "mainnet/concepts/tee", router: "mainnet/concepts/tee" },
           { name: "多重签名", path: "mainnet/concepts/multisigs", router: "mainnet/concepts/multisigs" },
           */
-        ]},
-        { name: "MTG", child: [
-          { name: "简介", path: "mainnet/mtg/overview", router: "mainnet/mtg/overview" },
         ]},
         { name: "教程", child: [
           { name: "加入主网", path: "mainnet/tutorials/full-node-join", router: "mainnet/tutorials/full-node-join" },
