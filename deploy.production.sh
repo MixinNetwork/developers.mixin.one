0fa3a093d0c6349b92b47b62b0bd196a9b9d7198
Li Yuqing <im.yuqlee@gmail.com>
2020-12-23 11:56:36 +0800

    Add deploy shell

---
+++ deploy.production.sh
@@ -0,0 +1,18 @@
+rm -rf ./dist
+mkdir dist
+
+cd developers
+yarn install && yarn build
+cd ..
+mv developers/dist dist/developers
+
+gzip -k dist/developers/js/*.js
+for old in dist/developers/js/*.js.gz; do mv $old dist/developers/js/`basename $old .js.gz`.js; done
+gzip -k dist/developers/css/*.css
+for old in dist/developers/css/*.css.gz; do mv $old dist/developers/css/`basename $old .css.gz`.css; done
+
+gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/developers/css/*.css gs://developers.mixin.one/css/
+gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/developers/js/*.js gs://developers.mixin.one/js/
+rm dist/developers/css/*.css dist/developers/js/*.js
+
+gsutil -h "Cache-Control:public, max-age=31536000" -m cp -r dist/developers/* gs://developers.mixin.one
