4bd8225d56104f4aa53b2930b3f3841b8934e6b3
Li Yuqing <im.yuqlee@gmail.com>
2021-02-03 17:01:05 +0800

    Replace with app engine

--- deploy.production.sh
+++ deploy.production.sh
@@ -8,16 +8,9 @@ yarn install && yarn build
 cd ..
 mv developers/dist dist/developers
 
-gzip -k dist/developers/js/*.js
-for old in dist/developers/js/*.js.gz; do mv $old dist/developers/js/`basename $old .js.gz`.js; done
-gzip -k dist/developers/css/*.css
-for old in dist/developers/css/*.css.gz; do mv $old dist/developers/css/`basename $old .css.gz`.css; done
 SUM=`md5 -q dist/developers/index.html`
 mv dist/developers/index.html dist/developers/index.$SUM.html
 
-gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/developers/css/*.css gs://developers.mixin.one/css/
-gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/developers/js/*.js gs://developers.mixin.one/js/
-rm dist/developers/css/*.css dist/developers/js/*.js
-
-gsutil -h "Cache-Control:public, max-age=31536000" -m cp -r dist/developers/* gs://developers.mixin.one
-gsutil web set -m index.$SUM.html -e index.$SUM.html gs://developers.mixin.one
+cp app.one.yaml dist/app.yaml
+sed -i ''  "s/index.html/index.$SUM.html/g" dist/app.yaml || exit
+cd dist && gcloud app deploy app.yaml
