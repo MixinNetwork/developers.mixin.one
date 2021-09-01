578dc855390ec992c56a011c3c877840d9d4e0cf
Li Yuqing <im.yuqlee@gmail.com>
2021-09-01 13:08:54 +0800

    Fix deploy script

--- deploy.production.sh
+++ deploy.production.sh
@@ -3,10 +3,17 @@
 rm -rf ./dist
 mkdir dist
 
+git pull
+
 cd developers
 yarn install && yarn build
 cd ..
 mv developers/dist dist/developers
+
+cd developers-docs
+yarn install && yarn build
+cd ..
+mv developers-docs/build dist/developers-docs
 
 SUM=`md5 -q dist/developers/index.html`
 mv dist/developers/index.html dist/developers/index.$SUM.html
