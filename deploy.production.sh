574d516f17a5b4aca981a6aec8943953fe444094
Li Yuqing <im.yuqlee@gmail.com>
2021-09-08 22:09:14 +0800

    Fix deploy shell

--- deploy.production.sh
+++ deploy.production.sh
@@ -19,5 +19,5 @@ SUM=`md5 -q dist/developers/index.html`
 mv dist/developers/index.html dist/developers/index.$SUM.html
 
 cp app.one.yaml dist/app.yaml
-sed -i ''  "s/index.html/index.$SUM.html/g" dist/app.yaml || exit
+#sed -i ''  "s/index.html/index.$SUM.html/g" dist/app.yaml || exit
 cd dist && gcloud app deploy app.yaml
