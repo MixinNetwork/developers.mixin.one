f6ba4a6ed97d228babbd63917e483215cbc6d69f
hundredark <hundredark@gmail.com>
2022-08-26 17:02:33 +0800

    Fix secret modal style

--- developers/src/components/Modals/SecretModal.vue
+++ developers/src/components/Modals/SecretModal.vue
@@ -1,18 +1,23 @@
 <template>
   <d-modal :show="showSecret">
-    <div class="new-secret-modal">
-      <h3>{{secretTitle}}</h3>
-      <span :class="type==='UpdateSession' && 'session'">{{secretContent}}</span>
-      <div class="btns">
-        <button v-if="type==='UpdateSecret'" @click="useCloseModal" class="btn-close primary">{{t('button.cancel')}}</button>
-        <button v-if="type==='UpdateSession'" @click="useClickDownload" class="btn-download primary">{{t('button.download')}}</button>
+    <div class="secret-modal">
+      <h3 class="title">{{secretTitle}}</h3>
+      <span
+        :class="[
+          'content',
+          type === 'UpdateSession' ? 'session' : ''
+        ]"
+      >{{secretContent}}</span>
+      <div class="button-container">
+        <button v-if="type==='UpdateSecret'" @click="useCloseModal" class="cancel">{{t('button.cancel')}}</button>
+        <button v-if="type==='UpdateSession'" @click="useClickDownload" class="cancel">{{t('button.download')}}</button>
         <button
           @click="useClickCopy"
           class="btn-copy primary"
         >{{t('button.copy')}}
         </button>
       </div>
-      <img @click="useCloseModal" class="iconguanbi" src="@/assets/img/svg/close.svg" alt="modal-close-icon"/>
+      <img @click="useCloseModal" class="close-modal-button" src="@/assets/img/svg/close.svg" alt="modal-close-icon"/>
     </div>
   </d-modal>
 </template>
@@ -53,24 +58,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.new-secret-modal {
+.secret-modal {
   text-align: center;
   padding: 3.125rem 3.75rem;
   max-width: 38.5rem;
 
-  h3 {
+  .title {
     margin-bottom: 1.3125rem;
     font-size: 1rem;
   }
 
-  span {
-    text-align: left;
-    word-wrap: break-word;
-    display: block;
-    padding: 1.625rem 1.25rem;
-    line-height: 1rem;
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 5.125rem;
+    padding: 1.625rem 1.25rem;
+    text-align: left;
+    line-height: 1rem;
+    word-break: break-all;
     background-color: #f6f9ff;
 
     &.session {
@@ -78,59 +85,35 @@ export default {
       overflow: auto;
     }
   }
-}
 
-.btns {
-  display: flex;
-  justify-content: flex-end;
+  .button-container {
+    display: flex;
+    justify-content: flex-end;
 
-  button {
-    width: 8.75rem;
-    height: 2.5rem;
-    margin-top: 2rem;
-    font-size: 0.9375rem;
-
-    &:hover {
-      opacity: 0.8;
+    button {
+      width: 8.75rem;
+      height: 2.5rem;
+      margin: 2rem 0.5625rem 0 0;
+      font-size: 0.9375rem;
     }
-  }
-
-  .btn-download,
-  .btn-close {
-    background-color: #e5e7ec;
-    color: #333;
-    margin-right: 0.5625rem;
-  }
-}
-
-.iconguanbi {
-  position: absolute;
-  top: 1.875rem;
-  right: 1.875rem;
-  color: #b8bdc7;
-  cursor: pointer;
-  padding: 0.3125rem;
-
-  &:hover {
-    color: rgba(50, 119, 255, 0.8);
   }
 }
 
 @media screen and (max-width: 48rem) {
-  .new-secret-modal {
+  .secret-modal {
     padding: 2.5rem 1.25rem;
     max-width: 18.75rem;
 
-    h3 {
+    .title {
       margin-bottom: 1rem;
     }
 
-    span {
+    .content {
       padding: 0.625rem;
       font-weight: 500;
     }
 
-    img {
+    .close-modal-button {
       top: 1rem;
       right: 1rem;
     }
