bbdd5e0a7bf695ca1a35703945929bd45b75cd28
hundredark <hundredark@gmail.com>
2022-08-26 17:18:38 +0800

    Fix update token style

--- developers/src/components/Modals/UpdateTokenModal.vue
+++ developers/src/components/Modals/UpdateTokenModal.vue
@@ -1,39 +1,39 @@
 <template>
   <d-modal :show="show">
-    <div class="main modal">
-      <h3>{{ t('wallet.update_token') }}</h3>
-      <div class="edit-item">
+    <div class="container">
+      <h3 class="title">{{ t('wallet.update_token') }}</h3>
+      <div class="form-item">
         <label>Session ID</label>
         <input v-model="session_id" />
       </div>
-      <div class="edit-item">
+      <div class="form-item">
         <label>Pin Token</label>
         <input v-model="pin_token" />
       </div>
-      <div class="edit-item">
+      <div class="form-item">
         <label>Private Key</label>
         <textarea v-model="private_key"></textarea>
       </div>
-      <div class="edit-item intro-container">
+      <div class="form-item intro-container">
         <label></label>
         <div class="intro">{{ t('wallet.secret_intro') }}</div>
       </div>
-      <div class="btns">
+      <div class="button-container">
         <button
           @click="useClickSubmit"
-          class="btns-save primary"
+          class="primary"
         >
           {{ t('button.save') }}
         </button>
         <button
           @click="useClickCancel"
-          class="btns-cancel primary"
+          class="cancel"
         >
           {{ t('button.cancel') }}
         </button>
       </div>
       <img
-        class="iconguanbi"
+        class="close-modal-button"
         src="@/assets/img/svg/close.svg"
         alt="close-icon"
         @click="useClickCancel" />
@@ -76,101 +76,91 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.main {
+.container {
   padding: 3.125rem 2.5rem 2.5rem 2.5rem;
   overflow: hidden;
-}
 
-h3 {
-  font-size: 1rem;
-  margin: 0.625rem 0 2.5rem 0;
-  text-align: center;
-}
-
-label {
-  font-weight: 700;
-  min-width: 5.625rem;
-  margin-right: 20px;
-}
-
-.edit-item {
-  display: flex;
-  align-items: center;
-  margin-bottom: 1.25rem;
-
-  input {
-    width: 100%;
-    max-width: 492px;
-    height: 60px;
+  .title {
     font-size: 1rem;
-    background: #f6f9ff;
-    padding: 0 10px;
-    border-radius: 4px;
-    box-shadow: 0 1px 4px 0 rgba(28, 77, 174, 0.1);
+    margin: 0.625rem 0 2.5rem 0;
+    text-align: center;
   }
 
-  textarea {
-    font-size: 1rem;
-    padding: 0.625rem;
-    width: 30.75rem;
-    height: 5.125rem;
-    background: #f6f9ff;
+  .form-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1.25rem;
+
+    label {
+      margin-right: 20px;
+      font-weight: 700;
+      min-width: 5.625rem;
+    }
+
+    input {
+      width: 100%;
+      max-width: 492px;
+      height: 60px;
+      padding: 0 10px;
+      background: #f6f9ff;
+      border-radius: 4px;
+      box-shadow: 0 1px 4px 0 rgba(28, 77, 174, 0.1);
+      font-size: 1rem;
+    }
+
+    textarea {
+      width: 30.75rem;
+      height: 5.125rem;
+      padding: 0.625rem;
+      background: #f6f9ff;
+      font-size: 1rem;
+    }
+
+    &.intro-container {
+      margin-top: -0.5rem;
+
+      .intro {
+        color: #E04B4B;
+        font-size: 0.88rem;
+      }
+    }
   }
 
-  &.intro-container {
-    margin-top: -0.5rem;
+  .button-container {
+    margin-top: 4rem;
 
-    .intro {
-      color: #E04B4B;
+    button {
       font-size: 0.88rem;
+      width: 8.75rem;
+      height: 2.5rem;
+      float: right;
+      margin-left: 0.625rem;
     }
   }
-}
 
-.iconguanbi {
-  position: absolute;
-  top: 1.5625rem;
-  right: 1.5625rem;
-  color: #b8bdc7;
-  cursor: pointer;
-  padding: 0.3125rem;
-}
-
-.btns {
-  margin-top: 4rem;
-
-  button {
-    font-size: 0.88rem;
-    width: 8.75rem;
-    height: 2.5rem;
-    float: right;
-    margin-left: 0.625rem;
-  }
-
-  .btns-cancel {
-    background: #e5e7ec;
-    color: #333;
-
-    &:hover {
-      opacity: 0.8;
-    }
+  .close-modal-button {
+    top: 1.5625rem;
+    right: 1.5625rem;
   }
 }
 
 @media screen and (max-width: 48rem) {
-  .modal {
-    .iconguanbi {
+  .container {
+    width: 310px;
+    padding: 2.5rem 1.5625rem;
+
+    .close-modal-button {
       top: 1rem;
       right: 1rem;
     }
 
-    &.main {
-      padding: 2.5rem 1.5625rem;
-      width: 310px;
-
-      h3 {
+    .title {
         margin: 0 0 2rem 0;
-      }
+    }
+
+    .form-item {
+      flex-direction: column;
+      align-items: flex-start;
 
       label {
         font-weight: 700;
@@ -179,42 +169,32 @@ label {
         margin-bottom: 1rem;
       }
 
-      div {
-        flex-direction: column;
-
-        input {
-          height: 3.125rem;
-          width: 100%;
-        }
+      input {
+        height: 3.125rem;
+        width: 100%;
       }
 
-      .edit-item {
-        align-items: flex-start;
-
-        textarea {
-          width: 100%;
-          height: 3.125rem;
-        }
+      textarea {
+        width: 100%;
+        height: 3.125rem;
       }
 
-      .intro-container {
-
+      &.intro-container {
         label {
           display: none;
         }
       }
+    }
 
-      .btns {
-        margin: 0;
-      }
+    .button-container {
+      margin: 0;
 
-      .btns-save {
+      .primary {
         display: block;
         float: none;
         margin: 1.75rem auto 0 auto;
       }
-
-      .btns-cancel {
+      .cancel {
         display: none;
       }
     }
