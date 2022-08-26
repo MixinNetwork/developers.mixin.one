67fcc02c06b5730baa8fae8ecd6ba69035c26a6b
hundredark <hundredark@gmail.com>
2022-08-26 17:54:48 +0800

    Fix app-creation style

--- developers/src/pages/app-creation/index.vue
+++ developers/src/pages/app-creation/index.vue
@@ -1,23 +1,19 @@
 <template>
-  <div>
-    <d-header class="app-header">
-      <template #left>
-        <div class="header-back" @click="backward">
-          <img src="@/assets/img/app-svg/left.svg" alt="backward-icon"/>
-        </div>
-      </template>
-      <template #center>
-        <div>{{ t('dashboard.new_app') }}</div>
-      </template>
-    </d-header>
-    <header>
-      <div class="header-list">
-        <span class="header-item new-item">{{ t('dashboard.new_app') }}</span>
+  <d-header>
+    <template #left>
+      <div class="header-back" @click="backward">
+        <img src="@/assets/img/app-svg/left.svg" alt="backward-icon"/>
       </div>
-    </header>
-    <div class="dashboard-main">
-      <app-form />
-    </div>
+    </template>
+    <template #center>
+      <div>{{ t('dashboard.new_app') }}</div>
+    </template>
+  </d-header>
+  <header>
+    <span class="header-item new-item">{{ t('dashboard.new_app') }}</span>
+  </header>
+  <div class="dashboard-main">
+    <app-form />
   </div>
 </template>
 
@@ -52,89 +48,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.app-header {
-  display: none;
-}
-
 header {
+  max-width: 100%;
   height: 3.75rem;
+  padding: 1.6rem 1rem 0;
   background: rgba(255, 255, 255, 1);
   border-bottom: 0.0625rem solid #e1e7f5;
   color: #b8bdc7;
+  font-weight: 700;
 
-  .header-list {
+  .new-item {
     font-weight: 700;
-    width: 62rem;
-    max-width: 100%;
-    margin: 0 auto;
-    padding: 1.6rem 1rem 0;
-    white-space: nowrap;
-
-    .new-item {
-      font-weight: 700;
-      color: #1d69ff
-    }
-
-    .header-item {
-      margin-right: 5rem;
-      cursor: pointer;
-
-      &.header-item-active {
-        color: #1d69ff;
-        position: relative;
-
-        &::after {
-          content: '';
-          position: absolute;
-          bottom: -0.75rem;
-          left: 0;
-          height: 0.1875rem;
-          width: 1.875rem;
-          background: #3277ff;
-        }
-
-      }
-    }
+    color: #1d69ff
   }
 }
 
 .dashboard-main {
   width: 100%;
-  height: calc(100% - 3.75rem);
   overflow: auto;
 }
 
 @media screen and (max-width: 48rem) {
-  .dashboard-main {
-    height: calc(100% - 3rem);
-  }
-
-  .app-header {
-    display: block;
-  }
-
   header{
     display: none;
-  }
-
-  .header-back {
-    width: 3.125rem;
-    transform: translate(-1.5625rem);
-    padding-left: 1.5625rem;
-
-    img {
-      height: 1.5rem;
-      width: 1.5rem;
-      transform: translate(0, 0.3125rem)
-    }
-  }
-
-  .header-list {
-    justify-content: space-between;
-
-    .header-item {
-      margin: 0;
-    }
   }
 }
 </style>
