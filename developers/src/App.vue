5340e980c09f4e7f62a967dd8deaaf9f3a4fbefe
hundredark <hundredark@gmail.com>
2022-08-26 12:10:12 +0800

    Fix App style

--- developers/src/App.vue
+++ developers/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <component :is="layout" />
-  </div>
+  <component :is="layout" />
 </template>
 
 <script setup>
@@ -13,41 +11,6 @@ const layout = computed(() => `${route.meta.type}-layout`);
 </script>
 
 <style lang="scss">
+@import "@/assets/scss/common.scss";
 @import "@/assets/scss/theme.scss";
-
-.views {
-  position: absolute;
-  width: 100%;
-  transition: transform 0.8s ease, opacity 0.8s ease;
-  top: 0;
-}
-.slide-left-enter-active {
-  transition: transform 0.3s ease, opacity 0.3s ease;
-}
-.slide-left-leave-active {
-  transition: transform 0.4s ease, opacity 0.4s ease;
-}
-.slide-left-enter {
-  transform: translateX(100%);
-  opacity: 0;
-}
-.slide-left-leave-to {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-
-.slide-right-enter-active {
-  transition: transform 0.3s ease, opacity 0.3s ease;
-}
-.slide-right-leave-active {
-  transition: transform 0.4s ease, opacity 0.4s ease;
-}
-.slide-right-enter {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-.slide-right-leave-to {
-  transform: translateX(100%);
-  opacity: 0;
-}
 </style>
