30f48c380082c97794b1f9c73c4df99d1645f823
hundredark <hundredark@gmail.com>
2022-08-26 17:44:00 +0800

    Fix withdrawal modal style

--- developers/src/components/DHeader.vue
+++ developers/src/components/DHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="dashboard-app-header"
+    class="app-header"
     :style="has_shadow? 'box-shadow: 0px 1px 4px 0px rgba(28, 77, 174, 0.1);':''"
   >
     <span class="app-header-left">
@@ -32,39 +32,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dashboard-app-header {
-  background-color: #fff;
-  color: #333;
-  height: 44px;
+.app-header {
   width: 100%;
-  padding: 0 20px;
+  height: 44px;
+  display: none;
   position: relative;
+  padding: 0 20px;
+  color: #333;
+  background-color: #fff;
   line-height: 44px;
   font-size: 20px;
   font-weight: 700;
   z-index: 999;
+
   .app-header-left {
     position: absolute;
     left: 20px;
     top: 0;
   }
+
   .app-header-center {
     position: absolute;
     left: 50%;
     top: 0;
-    div {
-      max-width: 200px;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
     font-size: 18px;
     transform: translate(-50%);
   }
+
   .app-header-right {
     position: absolute;
     right: 20px;
     top: 0;
+  }
+}
+
+@media screen and (max-width: 48rem) {
+  .app-header {
+    display: block;
   }
 }
 </style>
