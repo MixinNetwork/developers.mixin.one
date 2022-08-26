31c658e52656ffae1cc437ba08d2424ec9b390a3
hundredark <hundredark@gmail.com>
2022-08-26 16:17:02 +0800

    Fix DModal style

--- developers/src/components/Modals/DModal.vue
+++ developers/src/components/Modals/DModal.vue
@@ -1,11 +1,11 @@
 <template>
-    <div v-if="show" class="modal">
-      <transition name="fade-up">
-        <div v-if="show" class="container" v-loading="loading">
-          <slot></slot>
-        </div>
-      </transition>
-    </div>
+  <div v-if="show" class="modal">
+    <transition name="fade-up">
+      <div v-if="show" class="container" v-loading="loading">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -25,23 +25,24 @@ export default {
 
 <style lang="scss" scoped>
 .modal {
-  background-color: rgba(0, 0, 0, 0.34);
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   bottom: 0;
   z-index: 999;
+  background-color: rgba(0, 0, 0, 0.34);
   user-select: none;
-}
-.container {
-  border-radius: 12px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  overflow: auto;
+
+  .container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: #fff;
+    border-radius: 12px;
+    overflow: auto;
+    transform: translate(-50%, -50%);
+  }
 }
 
 .fade-up-enter-active,
