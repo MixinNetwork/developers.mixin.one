40bf170024d14946b9d074a4391f0c46fe39ea16
li <im.yuqlee@gmail.com>
2025-12-17 12:23:54 +0800

    upgrade

--- developers/vite.config.mjs
+++ developers/vite.config.mjs
@@ -2,9 +2,6 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 import mkcert from 'vite-plugin-mkcert';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -29,26 +26,9 @@ export default defineConfig(({ mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-            process: true
-          }),
-          NodeModulesPolyfillPlugin()
-        ]
-      }
-    },
     build: {
       rollupOptions: {
         plugins: [
-          rollupNodePolyFill()
         ]
       }
     }
