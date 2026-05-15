653ef2fee69a43c45194f385c01ae6bed89cc371
YeungKC <11473691+YeungKC@users.noreply.github.com>
2026-05-15 16:06:36 +0900

    Resolve developer site security alerts (#1427)
    * Resolve developer site security alerts
    
    * Refine frontend security dependency updates

--- developers/src/utils/tools.js
+++ developers/src/utils/tools.js
@@ -12,10 +12,7 @@ function cmpBalance(a, b) {
 }
 
 export const getUrlParameter = (name) => {
-  name = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
-  const regex = new RegExp(`[\\?&]${name}=([^&#]*)`);
-  const results = regex.exec(window.location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  return new URLSearchParams(window.location.search).get(name) ?? '';
 };
 
 export const getMixinEnvironment = () => {
